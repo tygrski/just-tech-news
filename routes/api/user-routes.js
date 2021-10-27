578ce13a7ac69d14784c7b0519bee62fef9e2110
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-27 18:45:13 -0500

    user login route/password validation

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,29 @@ router.post('/', (req, res) => {
     });
 });
 
+// route to varify user's identity
+router.post('/login', (req, res) => {
+  // query operation, expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({ 
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({ message: "No user found with that email!"});
+      return;
+    }
+  
+    // verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if(!validPassword) {
+      res.status(400).json({ message: "Incorrect password"});
+      return;
+    }
+    res.json({ user: dbUserData, message: "You are not logged in !"})
+
+  });
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
