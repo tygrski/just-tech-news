abe952eee0632c0bd62be56124529df7eb9cebaa
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-28 11:32:48 -0500

    post-route bug

--- models/User.js
+++ models/User.js
@@ -52,12 +52,12 @@ User.init(
   },
   {
     hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
+        //pw hash enrypt, set up beforeCreate lifecycle "hook" functionality
         async beforeCreate(newUserData) {
           newUserData.password = await bcrypt.hash(newUserData.password, 10);
           return newUserData;
         },
-        // set up beforeUpdate  lifecycle "hook" functionality
+        //pw hash encrypt,  set up beforeUpdate  lifecycle "hook" functionality
         async beforeUpdate(updatedUserData) {
           updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
           return updatedUserData;
---
+++ routes/api/post-routes.js
@@ -0,0 +1,22 @@
+const router = ('router');
+const { Post, User } = require('../../models');
+
+// get all users,  route that will retrieve all posts in the database
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
